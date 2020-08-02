261a70bf4095e7ce00bc99d28adc4954a9ac48dd
Elizaaaaa <elizaaaaa@github.com>
2020-08-01 22:26:52 -0700

    sliding window find the first polynomials

--- laneline_finder.py
+++ laneline_finder.py
@@ -111,8 +111,77 @@ class LaneFinder():
         warped_img = cv2.warpPerspective(img, M, img_size, flags=cv2.INTER_LINEAR)
         return warped_img
 
-    def find_lane_pixels(img):
-        histogram = np.sum(warped_img, axis=0)
+    def find_lane_pixels(self, img, nwindows=9, margin=80, minpix=50, visualize=True):
+        histogram = np.sum(img[img.shape[0]//2:,:], axis=0)
+        out_img = np.dstack([img, img, img])
+        midpoint = np.int(histogram.shape[0] // 2)
+        left_base = np.argmax(histogram[:midpoint])
+        right_base = np.argmax(histogram[midpoint:]) + midpoint
+
+        window_height = np.int(img.shape[0]//nwindows)
+        nonzero = img.nonzero()
+        nonzeroy = np.array(nonzero[0])
+        nonzerox = np.array(nonzero[1])
+
+        leftx_current = left_base
+        rightx_current = right_base
+        left_lane_inds = []
+        right_lane_inds = []
+
+        for window in range(nwindows):
+            win_y_low = img.shape[0] - (window+1)*window_height
+            win_y_high = img.shape[0] - window*window_height
+            win_xleft_low = leftx_current - margin
+            win_xleft_high = leftx_current + margin
+            win_xright_low = rightx_current - margin
+            win_xright_high = rightx_current + margin
+
+            if visualize:
+                cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high), (0, 255, 0), 2)
+                cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), (0, 255, 0), 2)
+            
+            good_left_inds = ((win_y_low<=nonzeroy)&(nonzeroy<=win_y_high)&(win_xleft_low<=nonzerox)&(nonzerox<=win_xleft_high)).nonzero()[0]
+            good_right_inds = ((win_y_low<=nonzeroy)&(nonzeroy<=win_y_high)&(win_xright_low<=nonzerox)&(nonzerox<=win_xright_high)).nonzero()[0]
+            left_lane_inds.append(good_left_inds)
+            right_lane_inds.append(good_right_inds)
+            if (len(good_left_inds)>minpix):
+                leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+            if (len(good_right_inds)>minpix):
+                rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+        
+        try:
+            left_lane_inds = np.concatenate(left_lane_inds)
+            right_lane_inds = np.concatenate(right_lane_inds)
+        except ValueError:
+            pass
+
+        leftx = nonzerox[left_lane_inds]
+        lefty = nonzeroy[left_lane_inds]
+        rightx = nonzerox[right_lane_inds]
+        righty = nonzeroy[right_lane_inds]
+
+        return leftx, lefty, rightx, righty, out_img
+    
+    def fit_polynomial(self, img, visualize=True):
+        leftx, lefty, rightx, righty, out_img = self.find_lane_pixels(img)
+        left_fit = np.polyfit(lefty, leftx, 2)
+        right_fit = np.polyfit(righty, rightx, 2)
+        ploty = np.linspace(0, img.shape[0]-1, img.shape[0])
+
+        try:
+            left_fitx = left_fit[0]*ploty**2+left_fit[1]*ploty+left_fit[2]
+            right_fitx = right_fit[0]*ploty**2+right_fit[1]*ploty+right_fit[2]
+        except TypeError:
+            print('fit_polynomial: failed to fit a line!')
+
+        if visualize:
+            out_img[lefty, leftx] = [255,0,0]
+            out_img[righty, rightx] = [0,0,255]
+            plt.figure()
+            plt.plot(left_fitx, ploty, color='yellow')
+            plt.plot(right_fitx, ploty, color='yellow')
+            plt.imshow(out_img)
+            plt.show()
 
 # * Detect lane pixels and fit to find the lane boundary.
     def detect_lane_boundary(self):
@@ -132,6 +201,7 @@ def run_pipeline(lane_finder, img):
     undist_img = lane_finder.distortion_correction(img)
     binary_img = lane_finder.generate_binary_image(undist_img)
     warped_img = lane_finder.rectify_binary_image(binary_img)
+    lane_finder.fit_polynomial(warped_img)
     plt.imshow(warped_img, cmap='gray')
     plt.show()
     return
